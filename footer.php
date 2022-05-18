782d57f68c3570e452d837216e3116e211d777a1
Kyndra Dess <cynderrealand@gmail.com>
2022-05-18 11:47:41 -0400

    s12-c1 Boite modale étape #1 transition CSS

--- footer.php
+++ footer.php
@@ -46,6 +46,14 @@ wp_nav_menu(array(
                     "link_before"=>$icone)); ?>
     <div class="footer__recherche"><?php get_search_form(); ?></div>
 </footer>
+
+<div class="boite__modale">
+    <button class="boite__modale__fermeture">X</button>
+    <p class="boite__modale_texte">
+        Ceci est un premier test de boîte modale
+    </p>
+</div>
+
 <?php wp_footer(); ?>
 </body>
 
